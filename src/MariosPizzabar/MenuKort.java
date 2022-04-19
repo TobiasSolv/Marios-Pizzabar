37f08e7e4a56de96e3a6b15ad9ba722144da198d
Tobias <tobiasurban@hotmail.com>
2022-04-19 14:33:08 +0200

    Added Menukort

---
+++ src/MariosPizzabar/MenuKort.java
@@ -0,0 +1,23 @@
+package MariosPizzabar;
+
+public class MenuKort {
+
+    void menuKort(){
+        System.out.println("""
+                1.  Vesuvio: tomatsauce, ost, skinke og oregano ...........................................57,-
+                2.  Amerikaner: Nyhed tomatsauce, ost, oksefars og oregano ................................53,-
+                3.  Cacciatore: tomatsauce, ost, pepperoni og oregano .....................................57,-
+                4.  Carbona: Nyhed tomatsauce, ost, kødsauce, spaghetti, cocktailpølser og oregano ........63,-
+                5.  Dennis: Nyhed tomatsauce, ost, skinke, pepperoni, cocktailpølser og oregano............65,-
+                6.  Bertil: tomatsauce, ost, bacon og oregano..............................................57,-
+                7.  Silvia: tomatsauce, ost pepperoni, rød peber, løg, oliven og oregano...................61,-
+                8.  Victoria: tomatsauce, ost skinke, ananas, champignon, løg og oregano...................61,-
+                9.  Toronfo Nyhed tomatsauce, ost,skinke, bacon, kebab, chili og oregano...................61,-
+                10. Capricciosa: tomatsauce, ost, skinke, champignon og oregano............................61,-
+                11. Hawai: tomatsauce, ost,skinke, ananas, oregano.........................................61,-
+                12. Le Blissola: tomatsauce, ost,skinke, rejer og oregano..................................61,-
+                13. Venezia: tomatsauce, ost skinke, bacon og oregano......................................61,-
+                14. Mafia: Nyhed tomatsauce, ost,pepperoni, bacon, løg og oregano..........................61,-
+                """);
+    }
+}
